8d9432bec984f3cc3bec8255b4ae4fc64b4211d5
Tomas Knapen <tknapen@gmail.com>
2018-01-26 14:59:01 +0100

    implemented prf stimulus in stim, started setting up experimental design. all parameters in json

--- exp/session.py
+++ exp/session.py
@@ -10,14 +10,14 @@ from exptools.core.session import EyelinkSession
 from exptools.core.staircase import ThreeUpOneDownStaircase
 
 from trial import AttSizeTrial
-
+from stim import AttSizeBGStim, PRFStim
 
 
 class AttSizeSession(EyelinkSession):
 
     def __init__(self, *args, **kwargs):
 
-        super(STSession, self).__init__(*args, **kwargs)
+        super(AttSizeSession, self).__init__(*args, **kwargs)
 
         # self.create_screen(full_screen=True, engine='pygaze')
 
@@ -31,131 +31,19 @@ class AttSizeSession(EyelinkSession):
 
         self.stopped = False
 
-    def create_background_stimuli(self):
-        ########################################################################################################################
-        # Binning over different eccentricities per X and mm
-        # Recalculating pixels from the degrees for rings
-        ########################################################################################################################
+    def create_stimuli(self):
+        self.bg_stim = AttSizeBGStim(session=self, 
+                        nr_rings=self.config['bg_stim_nr_rings'], 
+                        ecc_min=self.config['bg_stim_ecc_min'], 
+                        ecc_max=self.config['bg_stim_ecc_max'], 
+                        nr_blob_rows_per_ring=self.config['bg_stim_nr_blob_rows_per_ring'], 
+                        row_spacing_factor=self.config['bg_stim_ow_spacing_factor'])
 
-        # Ring positions
-        ring_bins = np.digitize(mm_cortex_from_fovea, np.linspace(ecc_min,mm_cortex_from_fovea.max(), conditions_rings, endpoint=False))
-        max_ring_positions = np.zeros((len(np.unique(ring_bins)), 1))
-        for bi in np.unique(ring_bins):
-            max_ring_positions[bi,:] = [ecc_range[ring_bins==bi].max()]
-
-
-        def degreestopixels_rings(conditions_rings):
-            ringposition_degrees = []
-            ringposition_pixels = []
-
-            for x in range (0,len(max_ring_positions)):
-                deg = max_ring_positions[x,0]
-                ringposition_degrees.append(deg)
-                pix = (deg*10) * 10.63074
-                ringposition_pixels.append(pix)
-
-            return ringposition_degrees, ringposition_pixels
-
-        self.ringposition_degrees, self.ringposition_pixels = degreestopixels_rings(conditions_rings)
-
-
-        ########################################################################################################################
-        # Binning over different eccentricities per X and mm
-        # Recalculating pixels from the degrees for blob amounts, locations and sizes
-        ########################################################################################################################
-
-        total_blob_rows = (rows_of_blobs * conditions_rings)+3
-        blob_bins = np.digitize(mm_cortex_from_fovea, np.linspace(ecc_min,mm_cortex_from_fovea.max(), total_blob_rows, endpoint=False))
-        max_blob_positions = np.zeros((len(np.unique(blob_bins)), 1))
-
-        for bi in np.unique(blob_bins):
-            max_blob_positions[bi,:] = [ecc_range[blob_bins==bi].max()]
-
-        def degreestopixels_blobs(total_blob_rows):
-            blobposition_degrees = []
-            blobposition_pixels = []
-
-            for x in range(0, len(max_blob_positions)):       # Minus first and last
-                deg = max_blob_positions[x, 0]
-                blobposition_degrees.append(deg)
-                pix = (deg * 10) * 10.63074
-                blobposition_pixels.append(pix)
-
-            return blobposition_degrees, blobposition_pixels
-
-        self.blobposition_degrees, self.blobposition_pixels = degreestopixels_blobs(max_blob_positions)
-
-    def create_trials(self):
-        """creates trials by creating a restricted random walk through the display from trial to trial"""
-
-        ########################################################################################################################
-        # Cortical Magnification factor & Eccentricities
-        ########################################################################################################################
-
-        def cm(ecc, mm_0, tau):
-            return mm_0 * np.exp(-ecc*tau)
-
-        mm0 = sym.Symbol('mm0')
-        ecc = sym.Symbol('ecc')
-        tau = sym.Symbol('tau')
-        sym.integrate(sym.exp(-ecc*tau), ecc)
-
-        ecc_nr = 20000
-        ecc_range = np.linspace(ecc_min,ecc_max,ecc_nr)
-        mm_0 = 12
-        tau = 0.5
-        fs = 14
-
-        cm_trace = cm(ecc_range, mm_0, tau)
-        mm_cortex_from_fovea = np.cumsum(cm_trace) / (ecc_nr/ecc_max)
-
-        ##################################################################################
-        ##
-        ##  Calculate saccade path for all trials
-        ##
-        ##################################################################################
-
-        saccade_amplitude_pix = self.deg2pix(self.config['saccade_amplitude'])
-        max_eccentricity_pix = self.deg2pix(self.config['max_eccentricity'])
-        
-        # fixation positions
-        present_pos = np.array([0, 0]) # center of screen?        
-        trial_saccade_target_positions = [present_pos]
-
-        trial_distractor_positions = [np.array([0, 0])]
-        saccade_distractor_directions = [[0,0]]
-
-        for t in range(self.config['n_trials']):
-            saccade_direction = np.random.rand() * 2.0 * np.pi
-            saccade_vector = np.array([np.sin(saccade_direction)*saccade_amplitude_pix, 
-                                        np.cos(saccade_direction)*saccade_amplitude_pix])
-            while np.linalg.norm(present_pos + saccade_vector) > max_eccentricity_pix:
-                saccade_direction = np.random.rand() * 2.0 * np.pi
-                saccade_vector = np.array([np.sin(saccade_direction)*saccade_amplitude_pix, 
-                                            np.cos(saccade_direction)*saccade_amplitude_pix])
-
-            which_distractor_direction = np.radians(np.random.choice([-self.config['distractor_deviation_angle'],0,self.config['distractor_deviation_angle']]))
-            distractor_vector = np.array([np.sin(saccade_direction + which_distractor_direction)*saccade_amplitude_pix, 
-                                            np.cos(saccade_direction + which_distractor_direction)*saccade_amplitude_pix])
-
-            # store this information in list for later use
-            trial_saccade_target_positions.append(present_pos + saccade_vector)
-            trial_distractor_positions.append(present_pos + distractor_vector)
-            saccade_distractor_directions.append([saccade_direction, which_distractor_direction])
-
-            # update present fixation position at the end of the trial
-            present_pos = present_pos + saccade_vector
-
-        self.trial_saccade_target_positions = np.array(trial_saccade_target_positions)
-        self.trial_distractor_positions = np.array(trial_distractor_positions)
-        self.saccade_distractor_directions = np.array(saccade_distractor_directions)
-
-
-        ##################################################################################
-        ##
-        ##  And, the stimuli
-        ##
-        ##################################################################################
+        self.prf_stim = PRFStim(session=self, 
+                        cycles_in_bar=self.config['prf_checker_cycles_in_bar'], 
+                        bar_width=self.config['prf_checker_bar_width'],
+                        tex_nr_pix=self.config['prf_checker_tex_nr_pix'],
+                        flicker_frequency=self.config['prf_checker_flicker_frequency'])
 
         size_fixation_pix = self.deg2pix(self.config['size_fixation_deg'])
         self.fixation = visual.GratingStim(self.screen,
@@ -177,6 +65,18 @@ class AttSizeSession(EyelinkSession):
             alignHoriz = 'center',
             color=(1,0,0))
         self.instruction.setSize((1200,50))
+
+
+    def create_trials(self):
+        """creates trials by setting up staircases for background task, and prf stimulus sequence"""
+
+        ##################################################################################
+        ##
+        ##################################################################################
+
+        for 
+
+
 
     def run(self):
         """run the session"""
--- exp/stim.py
+++ exp/stim.py
@@ -65,7 +65,13 @@ def mm_cortex_from_fovea(ecc_min, ecc_max, nr=1e4, mm_0=12, tau=0.5, fs=14):
 
 
 class AttSizeBGStim(object):
-    def __init__(self, nr_rings=3, ecc_min=0.1, ecc_max=5, nr_blob_rows_per_ring=24, row_spacing_factor=0.8, session, **kwargs):
+    def __init__(self, 
+                session, 
+                nr_rings=3, 
+                ecc_min=0.1, 
+                ecc_max=5, 
+                nr_blob_rows_per_ring=24, 
+                row_spacing_factor=0.8, **kwargs):
         
         total_nr_rows = nr_rings*(nr_blob_rows_per_ring+1)+1
 
@@ -104,11 +110,6 @@ class AttSizeBGStim(object):
         for i in range(nr_rings):
             self.repopulate_condition_ring_colors(condition_nr=i, color_balance=0.5)
 
-        # xys = [[pol2cart(ecc, pa) for pa in np.linspace(0, 2*np.pi,bpr)+np.random.rand()] for ecc, bpr in zip(rows_ecc_in_deg[2:], nr_blobs_per_ring[1:])]
-        # pl.figure()
-        # for i, xy in enumerate(xys):
-        #     pl.plot(np.array(xy)[:,0], np.array(xy)[:,1], 'ko', ms=100*blob_row_blob_sizes[i])
-
         # rings are easy, blobs are hard
         self.rings = [psychopy.visual.Circle(self.session.screen, 
                                             radius=rows_ecc_in_deg[rp]*self.session.pixels_per_degree, 
@@ -138,14 +139,43 @@ class AttSizeBGStim(object):
         self.element_array_stim.draw()
 
 class PRFStim(object):  
-    __init__(self, cycles_in_bar=5, bar_width=1./8., tex_nr_pix=2048, **kwargs):
-        self.tex = 
+    def __init__(self, session, 
+                        cycles_in_bar=5, 
+                        bar_width=1./8., 
+                        tex_nr_pix=2048, 
+                        flicker_frequency=6, 
+                        **kwargs):
+        self.session = session
+        self.cycles_in_bar = cycles_in_bar
+        self.bar_width = bar_width
+        self.tex_nr_pix = tex_nr_pix
+        self.flicker_frequency = flicker_frequency
 
+        bar_width_in_pixels = self.tex_nr_pix*self.bar_width
+        bar_width_in_radians = 2*np.pi*self.cycles_in_bar
+        bar_pixels_per_radian = bar_width_in_radians/bar_width_in_pixels
+        pixels_ls = np.linspace(-tex_nr_pix/2*bar_pixels_per_radian,tex_nr_pix/2*bar_pixels_per_radian,tex_nr_pix)
 
+        tex_x, tex_y = np.meshgrid(pixels_ls, pixels_ls)
+        self.sqr_tex = np.sign(np.sin(tex_x) * np.sin(tex_y))
+        self.sqr_tex[:,self.tex_nr_pix*(bar_width/2)+self.tex_nr_pix/2:] = 0
+        self.sqr_tex[:,:-self.tex_nr_pix*(bar_width/2)+self.tex_nr_pix/2] = 0
 
+        #INITIALISE SOME STIMULI
+        self.checkerboard = visual.GratingStim(self.session.screen, tex=self.sqr_tex, mask=None,
+                          color=[1.0, 1.0, 1.0],
+                          opacity=1.0,
+                          size=(self.tex_nr_pix, self.tex_nr_pix),
+                          ori=0)
 
+    def draw(time=0, period, orientation, position):
+        checkerboard_polarity = np.sign(np.sin(2*np.pi*time*self.flicker_frequency))
 
+        self.checkerboard.setTex(self.sqr_tex*checkerboard_polarity)
+        self.checkerboard.setPos([-self.tex_nr_pix/2 + int(0.5+time)*self.tex_nr_pix/period, 0])
+        self.checkerboard.setOri(orientation)
 
+        self.checkerboard.draw()
 
 
 
